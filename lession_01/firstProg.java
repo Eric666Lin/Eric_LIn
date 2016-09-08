6461a10ae727a15036513419d024926d98744c84
Eric666Lin <ericvictor123@gmail.com>
2016-09-08 10:50:36 -0700

    added lession_01

---
+++ lession_01/firstProg.java
@@ -0,0 +1,14 @@
+public class firstProg
+{
+	public static void main (String[]args)
+	{
+		System.out.println("d88888b   d8888b.     d888888b    .o88b.  ");
+		System.out.println("88'            88      `8D      `88'      d8P    Y8  ");
+		System.out.println("88ooooo   88oobY'           88       8P       ");
+		System.out.println("88ooooo   88`8b              88       8b       ");
+		System.out.println("88.            88      `88.     .88.      Y8b    d8  ");
+		System.out.println("Y88888P   88         YD Y888888P    `Y88P'  ");
+	}
+}
+
+
---
+++ lession_01/secondProg.java
@@ -0,0 +1,13 @@
+public class secondProg
+{
+	public static void main (String[]args)
+	{
+		System.out.println("  ssss.  	 d    d  	 sssss    	 ssss.  	 sssss   	    ssssssss         ss          ssssssss  ");
+		System.out.println(" S       	 S    S 	 S     S  	 S             S     S   	    ss       ss         ss         ss            ");
+		System.out.println("S        	 S    S 	S       S 	 S      	S      S          ss       ss         ss         ss        ");
+		System.out.println("S        	 S ssss 	S       S 	 S            S       S 	    ssssssss         ss         sssssssss             	");
+		System.out.println("S       	  S    S	S       S	 S            S       S	    ss                   ss         ss        ");
+		System.out.println(" S      	  S    S  	 S     S   	 S           	S     S           ss                   ss         ss               ");
+		System.out.println("  sss P  	  P    s    	    ss         sssss      	sssss            ss	          ss         sssssssss       ");    
+	}
+}
