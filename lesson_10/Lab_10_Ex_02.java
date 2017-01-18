d8b38801852e46127a52a69eb02cd846097f0d54
Lin_Eric <ericvictor123@gmail.com>
2017-01-18 11:47:21 -0800

    lab 10

---
+++ lesson_10/Lab_10_Ex_02.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+
+public class  Lab_10_Ex_02 
+{
+	public static void main(String[]args) 
+	{
+		Scanner kb = new Scanner(System.in);
+		
+		System.out.println("Please enter an equation: ");
+		String expression = kb.nextLine();
+		
+		ArrayList<String> equation = new ArrayList<String>(Arrays.asList(expression.split(" ")));
+		System.out.println(doEquation(equation));
+	}
+	
+	public static ArrayList<String> doEquation(ArrayList<String> equation) 
+	{
+		int i = 0;
+		while (i < equation.size()) 
+		{
+			if ((equation.get(i).equals("*") || equation.get(i).equals("/")) && i < equation.size()) 
+				
+			{
+				if (equation.get(i).equals("*")) 
+				{
+					equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) * Integer.parseInt(equation.get(i+1))));
+					
+					equation.remove(i-1);
+					equation.remove(i);
+				}
+				else 
+				{
+					equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) / Integer.parseInt(equation.get(i+1))));
+					
+					equation.remove(i-1);
+					equation.remove(i);
+				}
+			}
+			else i++;
+		}
+		
+		while (i < equation.size()) 
+		{
+			if ((equation.get(i).equals("+") || equation.get(i).equals("-")) && i < equation.size())
+				{
+				if (equation.get(i).equals("+")) 
+				{
+					equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) + Integer.parseInt(equation.get(i+1))));
+					
+					equation.remove(i-1);
+					equation.remove(i);
+				}
+				else 
+				{
+					equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) - Integer.parseInt(equation.get(i+1))));
+					
+					equation.remove(i-1);
+					equation.remove(i);
+				}
+			}
+			else i++;
+		}
+		return equation;
+	}
+}
